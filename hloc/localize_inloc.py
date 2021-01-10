22335599447468cc51de6e8b38007a98e6d73ad8
Qunjie Zhou <jennyzqj00@gmail.com>
2021-01-10 11:37:05 +0100

    Implement customiszed interface for matchers

--- hloc/localize_inloc.py
+++ hloc/localize_inloc.py
@@ -1,3 +1,5 @@
+import os
+import time
 import argparse
 from pathlib import Path
 import numpy as np
@@ -110,6 +112,134 @@ def pose_from_cluster(dataset_dir, q, retrieved, feature_file, match_file,
     ret['cfg'] = cfg
     return ret, all_mkpq, all_mkpr, all_mkp3d, all_indices, num_matches
 
+def pose_from_cluster_onfly_matching(dataset_dir, q, retrieved, 
+                                     matcher, poses, rthres, skip_matches):
+    """Instead of loading prestored keypoints and matches, as in 
+    pose_from_cluster(), this function allows one to perform matching 
+    on fly with a predefined matcher.    
+    """    
+    imq_path = os.path.join(dataset_dir, q)    
+    height, width = cv2.imread(imq_path).shape[:2]
+    focal = 4032. * 28. / 36.  
+    cfg = {
+        'model': 'SIMPLE_PINHOLE',
+        'width': width,
+        'height': height,
+        'params': [focal, .5 * width, .5 * height]
+    }
+    
+    all_mkpq = []
+    all_mkpr = []
+    all_mkp3d = []
+    all_indices = []
+    num_matches = 0
+    times = []
+    failed = []
+    for i, r in enumerate(retrieved):
+        pair = names_to_pair(q, r)
+        imr_path = os.path.join(dataset_dir, r)       
+        
+        try:
+            # Matching
+            t0 = time.time()
+            matches, _ , _ = matcher(imq_path, imr_path)
+            times.append(time.time() - t0)
+            
+            if len(matches) < skip_matches:
+                failed.append((q, r))
+                continue
+
+            mkpq, mkpr = matches[:, :2], matches[:, 2:]
+            scan_r = loadmat(os.path.join(dataset_dir, r + '.mat'))["XYZcut"]
+            mkp3d, valid = interpolate_scan(scan_r, mkpr)
+            Tr = get_scan_pose(dataset_dir, r)
+            mkp3d = (Tr[:3, :3] @ mkp3d.T + Tr[:3, -1:]).T
+        except:
+            failed.append((q, r))
+            continue
+            
+        num_matches += len(mkpq)
+        all_mkpq.append(mkpq[valid])
+        all_mkpr.append(mkpr[valid])
+        all_mkp3d.append(mkp3d[valid])
+        all_indices.append(np.full(np.count_nonzero(valid), i))
+    if num_matches == 0:
+        return None, failed, times
+    all_mkpq = np.concatenate(all_mkpq, 0)
+    all_mkpr = np.concatenate(all_mkpr, 0)
+    all_mkp3d = np.concatenate(all_mkp3d, 0)
+    all_indices = np.concatenate(all_indices, 0)
+
+    ret = pycolmap.absolute_pose_estimation(all_mkpq, all_mkp3d, cfg, rthres)
+    ret['cfg'] = cfg        
+    poses[q] = (ret['qvec'], ret['tvec'])
+    res_dict =  {
+            'db': retrieved,
+            'PnP_ret': ret,
+            'keypoints_query': all_mkpq,
+            'keypoints_db': all_mkpr,
+            '3d_points': all_mkp3d,
+            'indices_db': all_indices,
+            'num_matches': num_matches,            
+    }
+    return res_dict, failed, times
+
+def localize_with_matcher(matcher, dataset_dir, retrieval_pairs, 
+                          odir, method_tag, rthres=48, skip_matches=20):
+    
+    # Query and retrieval pairs
+    retrieval_dict = parse_retrieval(retrieval_pairs)
+    queries = list(retrieval_dict.keys())
+    
+    # Output paths: result file and log for visualization
+    logging.info('Output dir: ' + odir)
+    if not os.path.exists(odir):
+        os.makedirs(odir) 
+    res_path = os.path.join(odir, f'{method_tag}.txt')        
+    logs_path = os.path.join(odir, f'{method_tag}.txt_logs.pkl')
+
+    # Initialize log data    
+    logs = {'odir': odir,
+            'method': method_tag,
+            'pairs': retrieval_pairs,
+            'skip_matches' : skip_matches,
+            'rthres': rthres,        
+            'loc': {}, 
+            'failed':[]
+           }
+
+    logging.info('Starting localization...')
+    poses = {}
+    match_times = []
+    num_3d = []
+    irat = []
+    for q in tqdm(queries):
+        retrieved = retrieval_dict[q]
+        res_dict, failed, times = pose_from_cluster_onfly_matching(
+            dataset_dir, q, retrieved, matcher, poses, 
+            rthres=rthres, skip_matches=skip_matches
+        )
+        logs['loc'][q] = res_dict
+        logs['failed'] += failed
+        match_times += times
+        if res_dict:
+            num_3d.append(len(res_dict['keypoints_query']))
+            irat.append(np.mean(res_dict['PnP_ret']['inliers']))
+    logging.info('\nDone! Failed pairs: {} Match time: {:.2f}s Npts: {:.0f} irat:{:.2f}%'.format(len(logs['failed']), np.mean(match_times), np.mean(num_3d), 100 * np.mean(irat)))
+    logging.info(f'Writing poses to {odir}...')
+    with open(res_path, 'w') as f:
+        for q in queries:
+            if q not in poses:
+                continue
+            qvec, tvec = poses[q]
+            qvec = ' '.join(map(str, qvec))
+            tvec = ' '.join(map(str, tvec))
+            name = q.split("/")[-1]
+            f.write(f'{name} {qvec} {tvec}\n')
+    logging.info(f'Writing logs to {logs_path}...')
+    with open(logs_path, 'wb') as f:
+        pickle.dump(logs, f)
+    logging.info('Done!')
 
 def main(dataset_dir, retrieval, features, matches, results,
          skip_matches=None):
